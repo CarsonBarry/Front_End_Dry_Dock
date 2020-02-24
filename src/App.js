e525c3f4715e7472c4653ba2a763b736d789732c
Carson Barry <CarsonBarry374@gmail.com>
2020-02-24 17:01:07 -0600

    Structure Update and Additions
    Added drydock frame and relevant elements/css. Added Splash/Content frames, changed "drawer" to "footer" and added more functions for related buttons. Various CSS Cleanup.

--- src/App.js
+++ src/App.js
@@ -9,33 +9,71 @@ import "./css/framework/buttons.css";
 import "./css/structure.css";
 import "./css/framework/quickstyles.css";
 import "./css/styles.css";
+import "./css/framework/drydock.css";
 
 export default function App() {
   
 
+  /*/ Condenses splash screen to sidebar /*/
+  function splashToSidebar() {
+    /*/ Targets and toggles .sidebar on #splash /*/
+    var toggle = document.getElementById("splash");
+    toggle.classList.toggle("sidebar");
+  }
+
+  /*/ Expands content from 0px wide /*/
+  function contentDrawer() {
+    /*/ Targets and toggles .sidebar on #splash /*/
+    var toggle = document.getElementById("content");
+    toggle.classList.toggle("active");
+  }
+
   /*/ Opens and closes Drawer /*/
-  function test() {
+  function footerDrawer() {
     /*/ Targets and toggles .active on #drawer /*/
-    var test = document.getElementById("drawer");
-    test.classList.toggle("active");
+    var toggle = document.getElementById("footer");
+    toggle.classList.toggle("active");
+  }
+
+  function test() {
+    /*/ Targets and toggles .active on  /*/
+    var toggle = document.getElementById("elementID");
+    toggle.classList.toggle("active");
   }
 
   return (
     
-    /*/ Note: Only the IDs matter for this example /*/
-    /*/ All relevant CSS is in "css/styles.css" /*/
+  <div id="drydockframe" class="">
+    <div class="drydockpanel flex-jS">    
+      <button class="drydockbutton" onClick={splashToSidebar}>Splash</button>
+      <button class="drydockbutton" onClick={contentDrawer}>Content Drawer</button>
+      <button class="drydockbutton" onClick={footerDrawer}>Footer Drawer</button>
+      <button class="drydockbutton" onClick={test}>Test</button>
+      <button class="drydockbutton" onClick={test}>Test</button>
+      <button class="drydockbutton" onClick={test}>Test</button>
+      <button class="drydockbutton" onClick={test}>Test</button>
+      <button class="drydockbutton" onClick={test}>Test</button>
+    </div>
 
-    /*/ Grid Container /*/
+    {/*/ Grid Container /*/}
     <div id="main" class="App font-Din">
 
-      {/*/ Button just toggles .active on #drawer /*/}
-      <button onClick={test}>Test</button>
+      {/*/ Grid Item - Splash Screen /*/}
+      <div id="splash" class="bcolor-grey90 tcolor-white flex-jC-aiC">
+        <h1>Splash</h1>
+      </div>
+
+      {/*/ Grid Item - Content Area /*/}
+      <div id="content" class="bcolor-grey90 tcolor-white flex-jC-aiC">
+        <h1>Content</h1>
+      </div>
 
       {/*/ Grid Item - Drawer /*/}
-      <div id="drawer" class="bcolor-grey80 tcolor-white flex-jC">
-        <h1>Drawer</h1>
+      <div id="footer" class="bcolor-grey80 tcolor-white flex-jC-aiC">
+        <h1>Footer</h1>
       </div>
 
     </div>
+  </div>
   );
 }
