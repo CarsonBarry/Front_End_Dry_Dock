8c5127f2bf49d95118d05165772851e0f7b9ed8c
Carson Barry <CarsonBarry374@gmail.com>
2020-02-25 10:47:39 -0600

    Site Open Animation Implemented

--- src/App.js
+++ src/App.js
@@ -12,7 +12,13 @@ import "./css/styles.css";
 import "./css/framework/drydock.css";
 
 export default function App() {
-  
+  /*/ Opens Site /*/
+  function openSite() {
+    /*/ Calls functions that open site/*/
+    splashToSidebar();
+    contentDrawer();
+    footerDrawer();
+  }
 
   /*/ Condenses splash screen to sidebar /*/
   function splashToSidebar() {
@@ -42,38 +48,51 @@ export default function App() {
   }
 
   return (
-    
-  <div id="drydockframe" class="">
-    <div class="drydockpanel flex-jS">    
-      <button class="drydockbutton" onClick={splashToSidebar}>Splash</button>
-      <button class="drydockbutton" onClick={contentDrawer}>Content Drawer</button>
-      <button class="drydockbutton" onClick={footerDrawer}>Footer Drawer</button>
-      <button class="drydockbutton" onClick={test}>Test</button>
-      <button class="drydockbutton" onClick={test}>Test</button>
-      <button class="drydockbutton" onClick={test}>Test</button>
-      <button class="drydockbutton" onClick={test}>Test</button>
-      <button class="drydockbutton" onClick={test}>Test</button>
+    <div id="drydockframe" class="">
+      <div class="drydockpanel flex-jS">
+        <button class="drydockbutton" onClick={splashToSidebar}>
+          Splash
+        </button>
+        <button class="drydockbutton" onClick={contentDrawer}>
+          Content Drawer
+        </button>
+        <button class="drydockbutton" onClick={footerDrawer}>
+          Footer Drawer
+        </button>
+        <button class="drydockbutton" onClick={openSite}>
+          Open Site
+        </button>
+        <button class="drydockbutton" onClick={test}>
+          Test
+        </button>
+        <button class="drydockbutton" onClick={test}>
+          Test
+        </button>
+        <button class="drydockbutton" onClick={test}>
+          Test
+        </button>
+        <button class="drydockbutton" onClick={test}>
+          Test
+        </button>
+      </div>
+
+      {/*/ Grid Container /*/}
+      <div id="main" class="App font-Din">
+        {/*/ Grid Item - Splash Screen /*/}
+        <div id="splash" class="bcolor-grey90 tcolor-white flex-jC-aiC">
+          <h1>Splash</h1>
+        </div>
+
+        {/*/ Grid Item - Content Area /*/}
+        <div id="content" class="bcolor-grey70 tcolor-white flex-jC-aiC">
+          <h1>Content</h1>
+        </div>
+
+        {/*/ Grid Item - Drawer /*/}
+        <div id="footer" class="bcolor-grey80 tcolor-white flex-jC-aiC">
+          <h1>Footer</h1>
+        </div>
+      </div>
     </div>
-
-    {/*/ Grid Container /*/}
-    <div id="main" class="App font-Din">
-
-      {/*/ Grid Item - Splash Screen /*/}
-      <div id="splash" class="bcolor-grey90 tcolor-white flex-jC-aiC">
-        <h1>Splash</h1>
-      </div>
-
-      {/*/ Grid Item - Content Area /*/}
-      <div id="content" class="bcolor-grey90 tcolor-white flex-jC-aiC">
-        <h1>Content</h1>
-      </div>
-
-      {/*/ Grid Item - Drawer /*/}
-      <div id="footer" class="bcolor-grey80 tcolor-white flex-jC-aiC">
-        <h1>Footer</h1>
-      </div>
-
-    </div>
-  </div>
   );
 }
